fa469958e128667cebd38386600db03019d8d8a7
Andrey Golovanov <networmix@gmail.com>
2020-04-26 00:25:27 +0100

    adding support for Iterable/Iterator protocol to the SinglyLinkedList

--- datastructures/linkedlist.py
+++ datastructures/linkedlist.py
@@ -1,7 +1,13 @@
 from . import helpers
 
 
-class Node(object):
+class SinglyLinkedListNode(object):
+    """
+    A node for a SinglyLinkedList
+    Each node contains a data field ("_value")
+    and a reference ("_next") to the next node in the list.
+    """
+
     def __init__(self, value=None):
         self._value = value
         self._next = None
@@ -19,14 +25,42 @@ class Node(object):
         self._next = node
 
 
-class SingleLinkedList(object):
+class SinglyLinkedList(object):
+    """
+    A linked list is a linear data structure, in which
+    the elements are not stored at contiguous memory locations.
+    In simple words, a linked list consists of nodes where each node contains a data field
+    and a reference(link) to the next node in the list.
+    """
+
     def __init__(self):
         self._head = None
         self._tail = None
+        self._next = None
         self._len = 0
 
     def __len__(self):
         return self._len
+
+    def __iter__(self):
+        next_elt = self._head
+        while next_elt:
+            tmp_next_elt = next_elt
+            next_elt = next_elt.get_next()
+            yield tmp_next_elt
+
+    def __next__(self):
+        if self._next is None:
+            self._next = self._head
+        while self._next:
+            tmp_next_elt = self._next
+            self._next = self._next.get_next()
+            return tmp_next_elt
+        raise StopIteration
+
+    def __str__(self):
+        list_elements = [str(elt.get_value()) for elt in self]
+        return f"{list_elements}"
 
     def _append(self, node):
         if not self._head:
@@ -38,21 +72,22 @@ class SingleLinkedList(object):
             self._tail = node
             self._len += 1
 
-    def _traverse(self):
-        queue = []
-        if self._head:
-            queue.append(self._head)
-        while queue:
-            elt = queue.pop()
-            next_elt = elt.get_next()
-            if next_elt is not None:
-                queue.append(next_elt)
-            yield elt
-    
-    def __str__(self):
-        list_elements = [str(elt.get_value()) for elt in self._traverse()]
-        return f"{list_elements}"
+    def _pop(self):
+        if self._len == 0:
+            raise IndexError
+        prev = None
+        cur = self._head
+        for node in iter(self):
+            prev = cur
+            cur = node
+        prev.set_next(None)
+        ret = self._tail
+        self._tail = prev
+        self._len -= 1
+        return ret
 
+    def pop(self):
+        return self._pop().get_value()
 
     def append(self, value):
-        self._append(Node(value))
+        self._append(SinglyLinkedListNode(value))
--- tests/test_linkedlist.py
+++ tests/test_linkedlist.py
@@ -3,18 +3,47 @@ from .context import linkedlist
 import pytest
 
 
-def test_create_node():
-    head = linkedlist.Node()
+def test_create_singlylinkedlistnode():
+    head = linkedlist.SinglyLinkedListNode()
     assert head
 
+
 def test_create_linkedlist():
-    ll = linkedlist.SingleLinkedList()
+    ll = linkedlist.SinglyLinkedList()
     assert len(ll) == 0
-    
-def test_linkedlist_append():    
+
+
+def test_linkedlist_append():
     test_dataset = [10, 11, 'a', 'b', None]
-    ll = linkedlist.SingleLinkedList()
+    ll = linkedlist.SinglyLinkedList()
     for elt in test_dataset:
         ll.append(elt)
     assert len(ll) == 5
+
+
+def test_linkedlist_pop():
+    test_dataset = [10, 11, 'a', 'b', None]
+    ll = linkedlist.SinglyLinkedList()
+    for elt in test_dataset:
+        ll.append(elt)
+    assert len(ll) == 5
+    assert ll.pop() == None
+    assert ll.pop() == 'b'
+    assert ll.pop() == 'a'
+    assert ll.pop() == 11
+    assert ll.pop() == 10
+    ll.append(0)
+    ll.append(1)
+    assert ll.pop() == 1
+    assert ll.pop() == 0
+    assert len(ll) == 0
+    with pytest.raises(IndexError):
+        assert ll.pop()
+
+
+def test_linkedlist_str():
+    test_dataset = [10, 11, 'a', 'b', None]
+    ll = linkedlist.SinglyLinkedList()
+    for elt in test_dataset:
+        ll.append(elt)
     assert str(ll) == "['10', '11', 'a', 'b', 'None']"
